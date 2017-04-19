880d3c003c11f5c347a520aa726882505dd2c58b
P266548 <p266548@zardoz.service.rug.nl>
2017-04-19 16:29:48 +0200

    ok

--- scripts/OMT_test.sh
+++ scripts/OMT_test.sh
@@ -12,7 +12,8 @@ FILE=$3
 test_script="/home/p266548/Documents/amr_Rik/Seq2seq/src/python/test_OMT_dir.py"
 
 if [ $2 = "per" ]; then						# we are on peregrine, load modules
-    echo "Train on Peregrine..."
+    echo "Testing on Peregrine..."
+    module load requests/2.7.0-goolfc-2.7.11-Python-2.7.9
     module load cuDNN/5.0-CUDA-7.5.18
 	module load foss/2016a
 	echo "Loading modules complete"
