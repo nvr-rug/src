8a421b4058fb714a2ed511026cd0b1473db9119b
P266548 <p266548@lett-133-38.let.rug.nl>
2017-05-31 16:56:25 +0200

    ok:

--- python/test_OMT_dir.py
+++ python/test_OMT_dir.py
@@ -59,6 +59,7 @@ def process_dir(cp_direc):
 	with open(log_file, 'w') as f_out:
 		for root, dirs, files in os.walk(process_files):
 			for f in files:
+				print f
 				if f.endswith(args.test_ext):
 					f_path = os.path.join(root, f)
 					out_file = output_direc + '/' + f.replace(args.test_ext,args.prod_ext)
