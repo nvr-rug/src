ceba5f88bd7441eaf2de01a943f2dbfe6f455306
P266548 <p266548@lett-133-38.let.rug.nl>
2017-06-01 10:57:28 +0200

    ok

---
+++ scripts/OMT_batch_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=02:00:00
+#IGNORE THIS SBATCH --nodes=1
+#IGNORE THIS SBATCH --ntasks=12
+#SBATCH --mem=12G
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+
+#very ugly way of calling multiple SLURM scripts
+
+echo "th /home/p266548/Documents/amr_Rik/OpenNMT/translate.lua -src $1 -output $2 -model $3 -beam_size $4 -max_sent_length $5 $6 -gpuid 1 -fallback_to_cpu"
+
--- scripts/OMT_cpu_test.sh
+++ scripts/OMT_cpu_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time=01:00:00
+#SBATCH --time=02:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=2
 #SBATCH --mem=4G
