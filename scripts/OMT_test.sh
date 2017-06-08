ae32bd61151d5a101e3d9434dba2a52e5bf7ea00
P266548 <p266548@zardoz.service.rug.nl>
2017-06-08 15:12:32 +0200

    ok

--- scripts/OMT_test.sh
+++ scripts/OMT_test.sh
@@ -21,19 +21,25 @@ if [ $2 = "per" ]; then						# we are on peregrine, load modules
 	echo "Loading modules complete"
 	gpu="-gpu gpu"
 	#. /home/p266548/torch/install/bin/torch-activate
+elif [ $2 = "cpu" ]; then	
+	echo "Not on Peregrine, but on CPU, no modules loaded"
+	gpu=''
 else
 	echo "Not on Peregrine, but on GPU, no modules loaded"
 	gpu="-gpu gpu"
 fi	
 
+if [ -z "$prod_ext" ]; then
+	prod_ext=".seq.amr"
+fi
    
 if [ -f $FILE ]; then
    if [ $4 = "dev" ]; then
       echo "Model file exists, do testing with dev folder"
-      python $test_script -o $output_dev -test $src -dev $dev -to_process $4 -tf $FILE -beam_size $beamsize -max_sent_length $max_sent_length -repl $replace_unk -n_best $n_best -test_ext $test_ext -tgt_ext $tgt_ext $gpu
+      python $test_script -o $output_dev -test $src -dev $dev -to_process $4 -tf $FILE -beam_size $beamsize -max_sent_length $max_sent_length -repl $replace_unk -n_best $n_best -test_ext $test_ext -tgt_ext $tgt_ext $gpu -prod_ext $prod_ext
    elif [ $4 = "test" ]; then
       echo "Model file exists, do testing with test folder"
-      python $test_script -o $output -test $src -dev $dev -to_process $4 -tf $FILE -beam_size $beamsize -max_sent_length $max_sent_length -repl $replace_unk -n_best $n_best -test_ext $test_ext -tgt_ext $tgt_ext $gpu
+      python $test_script -o $output -test $src -dev $dev -to_process $4 -tf $FILE -beam_size $beamsize -max_sent_length $max_sent_length -repl $replace_unk -n_best $n_best -test_ext $test_ext -tgt_ext $tgt_ext $gpu -prod_ext $prod_ext
    else
       echo "Model file exists, but last argument ($4) must be dev or test"
    fi   
