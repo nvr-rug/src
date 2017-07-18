529ac08abd95c2e68a1a4ffdc772745194b13639
P266548 <p266548@zardoz.service.rug.nl>
2017-07-18 10:49:11 +0200

    ok

--- python/models_to_zardoz.py
+++ python/models_to_zardoz.py
@@ -7,17 +7,7 @@ import sys,re,argparse, os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", required=True, type=str, help="Directory with models")
-parser.add_argument('-e', required = False, type = int, default = 1, help="Only include folders starting from this epoch (default 1 = all)")
-parser.add_argument("-epochs", required = False, type=int, nargs = '+', help="Only do these specific epochs (if not done already)")
-parser.add_argument("-end", required = False, type=int, default = 25, help="If we encounter this epoch we process it, after which we stop")
 args = parser.parse_args()
-
-
-
-def argument_check():
-	if args.e != 1 and args.epochs:
-		print 'Dont do both a threshold and fixed number, exiting...'
-		sys.exit(0)
 
 
 if __name__ == '__main__':
@@ -27,19 +17,11 @@ if __name__ == '__main__':
 	while not stop:
 		models = [f for f in os.listdir(args.f) if os.path.isfile(os.path.join(args.f + '/', f))]	#get all models
 		
-		for m in models:
-			m_file = os.path.join(args.f, m)
-			model_ep  = int(re.search(r'epoch([\d]+)', m).group(1))
-			
-			if args.epochs:
-				if model_ep in args.epochs:
-					print 'Keep ep {0}'.format(model_ep)
-				else:
-					print 'Delete ep {0}'.format(model_ep)	
-			elif model_ep >= args.e:
-				print 'Keep ep {0}'.format(model_ep)
-			else:		
-				print 'Delete ep {0}'.format(model_ep)
-		
-		os.system("sleep 10")	
+		if m:
+			os.system("sleep 180")	#wait a bit until the model is fully saved
+			for m in models:
+				if 'model' in m: #extra check to ensure we do not accidentally delete other files
+					m_file = os.path.join(args.f, m)
+					os_call = 'python ~/hulp_scripts/zardoz_sender.py {0}'.format(m_file)
+					print os_call
 					
