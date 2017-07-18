92835a6709eb3c611fee1088738898e41e703dd8
P266548 <p266548@zardoz.service.rug.nl>
2017-07-18 11:04:00 +0200

    ok

--- python/models_to_zardoz.py
+++ python/models_to_zardoz.py
@@ -7,6 +7,7 @@ import sys,re,argparse, os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", required=True, type=str, help="Directory with models")
+parser.add_argument('-end', required = False, type = int, default = 25, help="Stop after we see this epoch")
 args = parser.parse_args()
 
 
@@ -17,10 +18,19 @@ if __name__ == '__main__':
 		models = [f for f in os.listdir(args.f) if os.path.isfile(os.path.join(args.f + '/', f))]	#get all models
 		
 		if models:
-			os.system("sleep 5")	#wait a bit until the model is fully saved
-			for m in models:
+			os.system("sleep 5")	#wait a bit until the model is fully saved, probably too much, but we have the time anyway
+			for m in models: 
 				if 'model' in m: #extra check to ensure we do not accidentally delete other files
-					m_file = os.path.join(args.f, m)
-					os_call = 'scp -r {0} p266548@zardoz.service.rug.nl:{0}/'.format(m_file)
-					print os_call
+					model_ep  = int(re.search(r'epoch([\d]+)', m).group(1))	#errors when fails, extra check
 					
+					m_file = os.path.join(args.f, m)
+					os_call = 'scp {0} p266548@zardoz.service.rug.nl:{0}'.format(m_file)
+					os.system(os_call)	#move file
+					os.system("sleep 5")
+					os.system("rm {0}".format(m_file))
+					
+					if model_ep >= args.end:
+						stop = True
+						break
+		
+		os.system("sleep 300") #sleep for 5 minutes before checking again, no need to do this continiously				
